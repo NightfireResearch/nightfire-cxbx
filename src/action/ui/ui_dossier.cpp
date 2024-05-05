b377b85534a2f9f1da44b5d2e0309d8dc84c16ef
Charlie Bruce <charliebruce@gmail.com>
2024-05-05 20:40:38 +0100

    UI Dossier WIP

---
+++ src/action/ui/ui_dossier.cpp
@@ -0,0 +1,114 @@
+#include "../helpers.h"
+
+// const M_ITEM ds_gadgets[14] = {
+
+//     {
+//         .iconHashcode = ICON_DS_GADGET_TASER,
+//         .title = GADGET_TASER_NAME,
+//         .description = GADGET_TASER_DESC,
+//         .identifier = 0x12,
+//         .enabled = 1,
+//         .descriptionWhenDisabled = TXT_NULL
+//     },
+//     {
+//         .iconHashcode = ICON_DS_GADGET_LASER,
+//         .title = GADGET_LASER_NAME,
+//         .description = GADGET_LASER_DESC,
+//         .identifier = 0x15,
+//         .enabled = 1,
+//         .descriptionWhenDisabled = TXT_NULL
+//     },
+//     {
+//         .iconHashcode = ICON_DS_GADGET_GRAPPLE,
+//         .title = GADGET_TASER_NAME,
+//         .description = GADGET_GRAPPLE_DESC,   
+//         .identifier = 0x03,
+//         .enabled = 1,
+//         .descriptionWhenDisabled = TXT_NULL 
+//     },
+
+//     // TODO: Finish me
+
+// };
+
+// ds_options: 002e0af8 (PS2 EU), ?? (Xbox)
+const M_ITEM ds_options[4] = {
+    {
+        .iconHashcode = ICON_DOSSIER_RECORDS,
+        .title = DOSSIER_RECORDS_NAME,
+        .description = DOSSIER_RECORDS_DESC,
+        .identifier = 0,
+        .enabled = 1,
+        .descriptionWhenDisabled = TXT_NULL
+    },
+    {
+        .iconHashcode = ICON_DOSSIER_REWARDS,
+        .title = DOSSIER_REWARDS_NAME,
+        .description = DOSSIER_REWARDS_DESC,
+        .identifier = 1,
+        .enabled = 1,
+        .descriptionWhenDisabled = TXT_NULL
+    },
+    {
+        .iconHashcode = ICON_DOSSIER_GADGETS,
+        .title = DOSSIER_GADGETS_NAME,
+        .description = DOSSIER_GADGETS_DESC,
+        .identifier = 2,
+        .enabled = 1,
+        .descriptionWhenDisabled = TXT_NULL
+    },
+    {
+        .iconHashcode = ICON_DOSSIER_WEAPONS,
+        .title = MP_CFG_OR_DOSSIER_WEAPONS,
+        .description = DOSSIER_WEAPONS_DESC,
+        .identifier = 3,
+        .enabled = 1,
+        .descriptionWhenDisabled = TXT_NULL
+    }
+};
+
+
+undefined4 C_SBDOSSIER_Handler(uchar param_1,M_CONTROL *param_2,uint param_3,uint param_4,int param_5,int param_6) {
+  long lVar1;
+  HASHCODE iVar2;
+  
+  if (param_4 == 0x4b) {
+    lVar1 = __Menu_SendMessage__FP9M_CONTROLUiii(param_2,0x40,0,0);
+    if (lVar1 == 1) {
+      iVar2 = MENU_DSREWARDS;
+    }
+    else {
+      if (lVar1 != 0) {
+        if (lVar1 == 2) {
+          Menu_ChangePageCloseIris(MENU_DSGADGETS,param_1,0x1000010b);
+          return 1;
+        }
+        if (lVar1 != 3) {
+          return 1;
+        }
+        Menu_ChangePageCloseIris(MENU_DSWEAPONS,param_1,0x1000010b);
+        return 1;
+      }
+      iVar2 = MENU_DSRECORDS;
+    }
+    Manager_SendMessage(manager + param_1,0x44,iVar2,0);
+    return 1;
+  }
+  if (param_4 < 0x4c) {
+    if (param_4 != 0x49) {
+      return 1;
+    }
+  }
+  else {
+    if (param_4 == 0x51) {
+      __Menu_SendMessage__FP9M_CONTROLUiii(param_2,0x27,0,3);
+      return 1;
+    }
+    if (param_4 != 0x54) {
+      return 1;
+    }
+  }
+  Menu_UpdateWheel(param_1,param_2,ds_options,0x1000010d,0x1000010a,0x100001ed,0x1000010b,
+                   param_4 == 0x49);
+  return 1;
+}
