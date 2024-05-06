ba41be3617bcf87dd9357b7d6dfc4d3a51f21ed5
Charlie Bruce <charliebruce@gmail.com>
2024-05-06 16:48:47 +0100

    Back to C++ but now specifying standard

--- src/action/ui/ui.h
+++ src/action/ui/ui.h
@@ -1,1 +1,2 @@
 long Handler_HandleMessage(uchar param_1, M_CONTROL *param_2, uint param_3, int param_4, int param_5);
+undefined4 C_SBDOSSIER_Handler(uchar param_1,M_CONTROL *param_2,uint param_3,uint param_4,int param_5,int param_6);
--- src/action/ui/ui_dossier.cpp
+++ src/action/ui/ui_dossier.cpp
@@ -67,48 +67,48 @@ const M_ITEM ds_options[4] = {
     }
 };
 
+typedef struct {
+  char unknown[0x76];
+} M_MANAGER;
 
-undefined4 C_SBDOSSIER_Handler(uchar param_1,M_CONTROL *param_2,uint param_3,uint param_4,int param_5,int param_6) {
-  long lVar1;
-  HASHCODE iVar2;
+#define manager ((M_MANAGER *)0x0025f1d0)
+
+// AUTOINJECT
+undefined4 C_SBDOSSIER_Handler(uchar param_1, M_CONTROL *param_2, uint param_3, uint param_4, int param_5, int param_6) {
   
-  if (param_4 == 0x4b) {
-    lVar1 = __Menu_SendMessage__FP9M_CONTROLUiii(param_2,0x40,0,0);
-    if (lVar1 == 1) {
-      iVar2 = MENU_DSREWARDS;
-    }
-    else {
-      if (lVar1 != 0) {
-        if (lVar1 == 2) {
-          Menu_ChangePageCloseIris(MENU_DSGADGETS,param_1,0x1000010b);
+  switch (param_4) {
+    case 0x4b: {
+      
+      int lVar1 = __Menu_SendMessage(param_2, 0x40, 0, 0);
+
+      switch(lVar1) {
+        case 0:
+          Manager_SendMessage(manager[param_1], 0x44, 0x4000003a, 0);
           return 1;
-        }
-        if (lVar1 != 3) {
+        case 1:
+          Manager_SendMessage(manager[param_1], 0x44, 0x4000003b, 0);
           return 1;
-        }
-        Menu_ChangePageCloseIris(MENU_DSWEAPONS,param_1,0x1000010b);
-        return 1;
+        case 2:
+          Menu_ChangePageCloseIris(MENU_DSGADGETS, param_1, 0x1000010b);
+          return 1;
+        case 3:
+          Menu_ChangePageCloseIris(MENU_DSWEAPONS, param_1, 0x1000010b);
+          return 1;
       }
-      iVar2 = MENU_DSRECORDS;
+      
     }
-    Manager_SendMessage(manager + param_1,0x44,iVar2,0);
+
+  case 0x49:
+  case 0x54:
+    Menu_UpdateWheel(param_1, param_2, ds_options, 0x1000010d, 0x1000010a, 0x100001ed, 0x1000010b, param_4 == 0x49);
     return 1;
-  }
-  if (param_4 < 0x4c) {
-    if (param_4 != 0x49) {
+
+  case 0x51:
+      __Menu_SendMessage(param_2, 0x27, 0, 3);
       return 1;
-    }
-  }
-  else {
-    if (param_4 == 0x51) {
-      __Menu_SendMessage__FP9M_CONTROLUiii(param_2,0x27,0,3);
+
+  default:
       return 1;
-    }
-    if (param_4 != 0x54) {
-      return 1;
-    }
   }
-  Menu_UpdateWheel(param_1,param_2,ds_options,0x1000010d,0x1000010a,0x100001ed,0x1000010b,
-                   param_4 == 0x49);
-  return 1;
+  
 }
