b79edc275119e5c249123f8125b2e31d76223f24
Charlie Bruce <charliebruce@gmail.com>
2025-02-03 23:07:54 +0000

    Fix Car_Init (incorrectly took a memory location from PS2)

--- src/action/game/obj/car.cpp
+++ src/action/game/obj/car.cpp
@@ -106,7 +106,7 @@ void Car_InitBits(CAR_INFO *tankInfo, obj_tag *baseObj) {
     celglist_tag * barrelCelgl = hashtable_hashcode_to_celglist(barrelHashcode);
 
     tankInfo->turretGeom = Control_CreateObjEx(0, Mat_Position(baseObj->transformMatrix), NULL, NULL, turretCelgl, baseObj, 0, 4, 1.0, 0x00, 0xff, 0xff, 0xff);
-    tankInfo->barrelGeom = Control_CreateObjEx(0, Mat_Position(baseObj->transformMatrix), NULL, NULL, barrelCelgl, tankInfo->turretGeom, 0, 4, 1.0, 0x20, 0xff, 0xff, 0xff);
+    tankInfo->barrelGeom = Control_CreateObjEx(0, Mat_Position(baseObj->transformMatrix), NULL, NULL, barrelCelgl, baseObj, 0, 4, 1.0, 0x20, 0xff, 0xff, 0xff);
 
     // Meaning currently unknown - render mode?
     tankInfo->turretGeom->someFlags_0xcc |= 0x40;
@@ -177,7 +177,7 @@ void Car_Activate(obj_tag* carObj, obj_tag* playerObj) {
 #endif
 
 // WIP
-// NOAUTOINJECT
+// AUTOINJECT
 obj_tag * Car_Create(_VECTOR *pos, _VECTOR *rot, celglist_tag *celgl, level_tag *level) {
 
     printf("Car_Create: Spawning at %f %f %f, miniVehiclesEnabled %i\n", pos->x, pos->y, pos->z, MPSettings.miniVehiclesEnabled);
--- src/action/game/obj/object.h
+++ src/action/game/obj/object.h
@@ -25,13 +25,14 @@ typedef struct obj_tag {
     char _pad_11111[0xc];
     _VECTOR position;
     char _pad_2[0x40];
-    _MATRIX transformMatrix;
-    char _pad_3[0x10];
-    void* extraObjectData;
+    _MATRIX transformMatrix; // 0x70 - 0xAC
+    char _pad_9999[4];
+    void* maybeCollision; // 0xB0
+    char _pad_3[0x8];
+    void* extraObjectData; // 0xBC
     char _pad_4[0xc];
     int someFlags_0xcc; // 0xCC, unclear what the meaning is but sometimes relevant for rendering or object state or straddle tests?
-    char _pad_5[0x4];
-    void* maybeCollision;
+    char _pad_5[0x8];
     char _pad_6[0x3];
     ObjectType objectType;
 } obj_tag;
@@ -40,9 +41,9 @@ typedef struct obj_tag {
 //char (*__kaboom)[offsetof(obj_tag,objectType)] = 1;
 static_assert(offsetof(obj_tag, position) == 0x24, "Offset of position not correct");
 static_assert(offsetof(obj_tag, transformMatrix) == 0x70, "Offset of transformMatrix not correct");
+static_assert(offsetof(obj_tag, maybeCollision) == 0xb0, "Offset of maybeCollision not correct");
 static_assert(offsetof(obj_tag, extraObjectData) == 0xbc, "Offset of extraObjectData not correct");
 static_assert(offsetof(obj_tag, someFlags_0xcc) == 0xcc, "Offset of someFlags_0xcc not correct");
-static_assert(offsetof(obj_tag, maybeCollision) == 0xd4, "Offset of maybeCollision not correct");
 static_assert(offsetof(obj_tag, objectType) == 0xdb, "Offset of objectType not correct");
 
 #endif // OBJECT_H_
