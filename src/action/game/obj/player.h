ab6f9f453831db72f208ad203e5dba4acbce4fcd
Charlie Bruce <charliebruce@gmail.com>
2025-02-04 03:24:11 +0000

    Missing file

---
+++ src/action/game/obj/player.h
@@ -0,0 +1,27 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+#include "object.h"
+
+#pragma pack(push, 1)
+
+// WIP
+typedef struct {
+    char _pad_1[0x808];
+    obj_tag* remoteControlDevice; // 0x808
+    char _pad_2[0xc6];
+    short previousSubState; //0x8d2
+    char _pad_3[0xa];
+    char playerNum; // 0x8de
+    // ...
+} BLData;
+
+
+//char (*__kaboom)[offsetof(BLData,playerNum)] = 1;
+static_assert(offsetof(BLData, remoteControlDevice) == 0x808, "Offset of remoteControlDevice not correct");
+static_assert(offsetof(BLData, playerNum) == 0x8de, "Offset of playerNum not correct");
+
+#pragma pack(pop)
+
+
+#endif // PLAYER_H_
