146d5470647c92e88b34940e89ae126d26e89dae
Charlie Bruce <charliebruce@gmail.com>
2025-03-25 00:13:28 +0000

    WIP on collision implementation - need floating-point fixes first

--- src/action/actionhelpers.h
+++ src/action/actionhelpers.h
@@ -1,35 +1,27 @@
 #ifndef ACTIONHELPERS_H
 #define ACTIONHELPERS_H
 
+// Foward declarations prevent circular dependencies
+typedef struct celglist_tag celglist_tag;
+typedef struct level_tag level_tag;
+typedef struct world_tag world_tag;
+typedef struct cel_tag cel_tag;
+typedef struct obj_tag obj_tag;
+
+// Hashcode, SFX and translated text names
 #include "assets.h"
+
+// Ghidra types
 #include "../helpers.h"
 
+#include "util/LList.h"
+#include "math/math.h"
+#include "engine/Collide.h"
 #include "game/obj/object.h" // for obj_tag needed by some autogen functions
 #include "game/obj/control.h" // for Control_X functions
 #include "game/obj/player.h" // for BLData
 #include "gfx/Sprite.h"
 #include "ui/ui.h"
 #include "ui/HUD.h"
-#include "util/LList.h"
-
-// TODO: Unfinished
-typedef struct {
-    char unknown;
-} celglist_tag;
-
-// TODO: Unfinished
-typedef struct {
-    char unknown;
-} level_tag;
-
-// TODO: Unfinished
-typedef struct {
-    char unknown;
-} world_tag;
-
-// TODO: Unfinished
-typedef struct {
-    char unknown;
-} cel_tag;
 
 #endif //ACTIONHELPERS_H
