82fe46d3cb75b46cdf88cc25089cbe44df623d61
Charlie Bruce <charliebruce@gmail.com>
2025-04-18 21:01:49 +0100

    Implement Camera_Create

--- src/action/engine/Camera.cpp
+++ src/action/engine/Camera.cpp
@@ -29,38 +29,47 @@ void Camera_ScreenCoords(ushort viewerNum,float PosX,float PosY,float Width,floa
 
 }
 
-// NOAUTOINJECT
-// void Camera_Create(int idx, world_tag *param_2, char param_3, ushort posX, ushort posY, ushort width, ushort height) {
-//   viewer_tag *viewer;
-//   cel_tag *pcVar1;
-//   world_tag *world;
-//   cel_tag *iVar1;
+// AUTOGEN
+void build_link_world_to_viewer(viewer_tag *viewer, cel_tag **world);
+
+// AUTOGEN
+viewer_tag* build_alloc_viewer(unsigned char idx);
+
+// AUTOGEN
+cel_tag* build_alloc_cel(cel_tag *cel);
+
+// AUTOINJECT
+void Camera_Create(int idx, world_tag *param_2, char param_3, ushort posX, ushort posY, ushort width, ushort height) {
+  viewer_tag *viewer;
+  cel_tag *pcVar1;
+  world_tag *world;
+  cel_tag *iVar1;
   
-//   if (glb_viewer[idx] == NULL) {
-//     world = param_2;
-//     viewer = build_alloc_viewer((char)idx);
-//     build_link_world_to_viewer(viewer,(cel_tag **)world);
-//     glb_viewer[idx] = viewer;
-//   }
-//   if (param_3 != NULL) {
-//                     /* Linked list insert at front? */
-//     pcVar1 = build_alloc_cel(*(cel_tag **)glb_viewer[idx]->world);
-//     *(cel_tag **)glb_viewer[idx]->world = pcVar1;
-//     iVar1 = *(cel_tag **)glb_viewer[idx]->world;
-//     iVar1->bitsFromPlacementTag = iVar1->bitsFromPlacementTag | 0x10040000;
-//     (iVar1->someMin).z = -1000.0;
-//     (iVar1->someMin).y = -1000.0;
-//     (iVar1->someMin).x = -1000.0;
-//     (iVar1->someMax).z = 1000.0;
-//     (iVar1->someMax).y = 1000.0;
-//     (iVar1->someMax).x = 1000.0;
-//   }
-//   glb_viewer[idx]->world = (cel_tag *)param_2;
-//   glb_viewer[idx]->field_0x29 = param_3;
-//   glb_viewer[idx]->field26_0x2a = 1;
-//   glb_viewer[idx]->cameraUpdateObj = NULL;
-//   glb_viewer[idx]->cameraUpdateCallback = NULL;
-//   Camera_Enable((ushort)idx,param_3,1,NULL);
-//   Camera_ScreenCoords((ushort)idx,(float)posX,(float)posY,(float)width,(float)height);
-//   return;
-// }
+  if (glb_viewer[idx] == NULL) {
+    world = param_2;
+    viewer = build_alloc_viewer(idx);
+    build_link_world_to_viewer(viewer,(cel_tag **)world);
+    glb_viewer[idx] = viewer;
+  }
+  if (param_3 != 0) {
+                    /* Linked list insert at front? */
+    pcVar1 = build_alloc_cel(*(cel_tag **)glb_viewer[idx]->world);
+    *(cel_tag **)glb_viewer[idx]->world = pcVar1;
+    iVar1 = *(cel_tag **)glb_viewer[idx]->world;
+    iVar1->bitsFromPlacementTag |= 0x10040000;
+    (iVar1->someMin).z = -1000.0;
+    (iVar1->someMin).y = -1000.0;
+    (iVar1->someMin).x = -1000.0;
+    (iVar1->someMax).z = 1000.0;
+    (iVar1->someMax).y = 1000.0;
+    (iVar1->someMax).x = 1000.0;
+  }
+  glb_viewer[idx]->world = (cel_tag *)param_2;
+  glb_viewer[idx]->field25_0x29 = param_3;
+  glb_viewer[idx]->field26_0x2a = 1;
+  glb_viewer[idx]->cameraUpdateObj = NULL;
+  glb_viewer[idx]->cameraUpdateCallback = NULL;
+  Camera_Enable(idx, param_3, 1, NULL);
+  Camera_ScreenCoords(idx, (float)posX, (float)posY, (float)width, (float)height);
+  return;
+}
