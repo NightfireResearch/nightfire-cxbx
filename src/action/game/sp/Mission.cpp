236deca7e16112ee6a850bcca4a90fb01f7a2527
Charlie Bruce <charliebruce@gmail.com>
2025-04-27 05:11:31 +0100

    Implement Mission_NumVisObjectives

--- src/action/game/sp/Mission.cpp
+++ src/action/game/sp/Mission.cpp
@@ -3,7 +3,10 @@
 
 #define FailLabel U32_AT(0x0017e548)
 #define BaseMap U32_AT(0x0025fe20)
+#define ThisOrderNum U32_AT(0x0025fe24)
 #define MissionState U32_AT(0x0017e544)
+
+#define MissionData (*(MissionData(*)[24])0x0017e180)
 
 // AUTOINJECT
 void Mission_SetFailLabel(Action_TranslatedText text) {
@@ -31,4 +34,29 @@ void Mission_SetStatus(undefined4 param_1) {
 // AUTOINJECT
 undefined4 Mission_Status(void) {
   return MissionState;
+}
+
+// AUTOINJECT
+int Mission_NumVisObjectives(void) {
+
+    int count = 0;
+
+    for(int i = 0; i < ARRAY_SIZE(MissionData); i++) {
+        if(MissionData[i].baseLevel != BaseMap)
+            continue;
+
+        if(ThisOrderNum < MissionData[i].idxInOrder) // The missions are sorted, if we reach a later mission then we are done
+            return count;
+
+        for(int j = 0; j < MissionData[i].numObjectives; j++) {
+            int objState = MissionData[i].objectives[j].status;
+
+            if(objState != 0 && objState != 1)
+                count++;
+        }
+
+        
+    }
+
+    return count;
 }
--- src/action/game/sp/Mission.h
+++ src/action/game/sp/Mission.h
@@ -3,13 +3,41 @@
 
 #include "../../actionhelpers.h"
 
+#pragma pack(push, 1)
+
+typedef struct {
+    Action_TranslatedText name;
+    uint unknown;
+    Action_TranslatedText description;
+    char someSwitchChannel;
+    char unknown1[2];
+    char someOtherSwitchChannel;
+    char someFlags; // ??
+    char unknown2[3];
+    int status;
+} Objective;
+
+static_assert(sizeof(Objective) == 0x18, "Size of Objective not correct");
+
+typedef struct {
+    HASHCODE level;
+    HASHCODE baseLevel;
+    uint idxInOrder;
+    Objective* objectives;
+    uint numObjectives;
+    char pad[20];
+} MissionData;
+
+static_assert(sizeof(MissionData) == 0x28, "Size of MissionData not correct");
+#pragma pack(pop)
+
 
 void Mission_SetFailLabel(Action_TranslatedText text);
 void Mission_SetMapHCode(HASHCODE param_1);
 HASHCODE Mission_BaseMapHCode(void);
 void Mission_SetStatus(undefined4 param_1);
 undefined4 Mission_Status(void);
-
+int Mission_NumVisObjectives(void);
 
 
 
