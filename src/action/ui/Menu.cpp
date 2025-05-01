4ffab487638839d8d0938e29e40a9e6a2a156869
Charlie Bruce <charliebruce@gmail.com>
2025-05-01 22:08:58 +0100

    Add missing Menu.cpp

---
+++ src/action/ui/Menu.cpp
@@ -0,0 +1,6 @@
+#include "Menu.h"
+
+// Menu (Send, SendEx, SendMessage), Iris (Start, Play), Wheel etc
+
+// AUTOGEN
+int Menu_GetLevelIndex(HASHCODE level);
---
+++ src/action/ui/Menu.h
@@ -0,0 +1,9 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "../../actionhelpers.h"
+
+int Menu_GetLevelIndex(HASHCODE level);
+
+
+#endif // MENU_H
