c4854ad45b1fd348d3d485003df91588e21ae3e8
Charlie Bruce <charliebruce@gmail.com>
2025-06-07 00:08:54 +0100

    Tidier Locks_Init implementation

--- src/action/game.cpp
+++ src/action/game.cpp
@@ -181,26 +181,22 @@ typedef struct {
 #define KeyCodes (*(KeyCodeEntry (*)[51])0x0029aaf8)
 
 // No need to inject, only called from function below
-void __stdcall Locks_Init(void) {
-  uint uVar1;
-  uint uVar5;
+void Locks_Init(void) {
   
   // Seed the random number generator
-  uVar1 = GameState.NumFramesUnpaused * 5 & 0xff;
-  uVar5 = 0;
-  if (uVar1 != 0) {
-    do {
+  for(int i = 0; i < GameState.NumFramesUnpaused * 10 & 0x1ff; i++) {
       Rand_Random();
-      uVar5 = uVar5 + 1;
-    } while ((uVar5 & 0xffff) < uVar1 << 1);
   }
 
+  // Set up each key code entry
   for(int i = 0; i < ARRAY_SIZE(KeyCodes); i++) {
 
     for (int digit = 0; digit < 4; digit++) {
       KeyCodes[i].asciiDigit[digit] = '0' + (char)(Rand_Random() % 10); // Original Game Bug: Previously was % 9, so '9' would never be in a keycode
     }
+
     KeyCodes[i].discovered = false;
+  
   }
 
 }
