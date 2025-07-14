f184a74d749727eb86490e05b6050cc952e3944c
Charlie Bruce <charliebruce@gmail.com>
2025-07-14 02:31:53 +0100

    Allow injection of Rand_FRandHalf

--- src/action/util/Random.cpp
+++ src/action/util/Random.cpp
@@ -5,16 +5,14 @@
 // AUTOGEN
 unsigned int Rand_Rand(unsigned int max);
 
+// AUTOGEN
+uint __stdcall Rand_Random(void);
 
-// UNINJECTABLE
+// AUTOINJECT
 float Rand_FRandHalf(float range) {
-
-    // Get a 32-bit random number from the RNG, and scale it to a float in the range 0,1
-    float randF = (float)(Rand_Rand(0x7fffffff) & 0x7fffffff) / (float)0x7fffffff;
-
-    // Rescale it to the range -range/2, range/2
-    return (randF * range) - (range * 0.5f);
-
+    uint rand_u32 = Rand_Random();
+    float rand_01 = rand_u32 * powf(2, -32); // Scale from range of uint32 to range 0, 1
+    return (rand_01 * range) - (range * 0.5f); // Output is in [-range/2, range/2]
 }
 
 // AUTOINJECT
@@ -24,15 +22,11 @@ void Rand_FRandHalf_Vec(_VECTOR *vecOut, float range) {
     vecOut->z = Rand_FRandHalf(range);
 }
 
-
-// AUTOGEN
-uint __stdcall Rand_Random(void);
-
 // AUTOINJECT
 float Float_FRand(float range) {
     uint rand_u32 = Rand_Random();
     float rand_01 = rand_u32 * powf(2, -32); // Scale from range of uint32 to range 0, 1
-    return rand_01 * range;
+    return rand_01 * range; // Output is in [0, range]
 }
 
 // AUTOGEN
