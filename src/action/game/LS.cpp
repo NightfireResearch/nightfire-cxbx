fb444133f481bfc0cee3912f76a1b0af606ce28c
Charlie Bruce <charliebruce@gmail.com>
2025-10-09 20:37:08 +0100

    Autoinject all the BIN_PullBits functions

--- src/action/game/LS.cpp
+++ src/action/game/LS.cpp
@@ -6,12 +6,26 @@
 void BIN_PullBits(uchar *data, uint *bitOffset, uint* dataSignOut, uchar numBits);
 
 // Helpers (these are all the same name in C++ land, differentiated only by type in the signature)
-void BIN_PullBits_U32(uchar *data, uint *bitOffset, uint *dataOut, uchar numBits) {
 
+// AUTOINJECT
+void BIN_PullBits_U32(uchar *data, uint *bitOffset, uint *dataOut, uchar numBits) {
     uint dataAndSign[2] = {0};
     BIN_PullBits(data, bitOffset, dataAndSign, numBits);
     *dataOut = dataAndSign[0];
+}
 
+// AUTOINJECT
+void BIN_PullBits_U16(uchar *data, uint *bitOffset, ushort *dataOut, uchar numBits) {
+    uint dataAndSign[2] = {0};
+    BIN_PullBits(data, bitOffset, dataAndSign, numBits);
+    *dataOut = dataAndSign[0];
+}
+
+// AUTOINJECT
+void BIN_PullBits_U8(uchar *data, uint *bitOffset, uchar *dataOut, uchar numBits) {
+    uint dataAndSign[2] = {0};
+    BIN_PullBits(data, bitOffset, dataAndSign, numBits);
+    *dataOut = dataAndSign[0];
 }
 
 // AUTOINJECT
--- src/action/game/LS.h
+++ src/action/game/LS.h
@@ -5,4 +5,9 @@
 
 void LS_LoadCheats(uchar *data);
 
+
+void BIN_PullBits_U32(uchar *data, uint *bitOffset, uint *dataOut, uchar numBits);
+void BIN_PullBits_U16(uchar *data, uint *bitOffset, ushort *dataOut, uchar numBits);
+void BIN_PullBits_U8(uchar *data, uint *bitOffset, uchar *dataOut, uchar numBits);
+
 #endif // LS_H_
