de3d85142cd9e8677b35aea405372a9f191f6e49
Charlie Bruce <charliebruce@gmail.com>
2025-11-27 17:11:37 +0000

    Break out DynamicObject and Env_Tree_Create. Parsemap now tidy

---
+++ src/action/game/obj/DynamicObject.h
@@ -0,0 +1,8 @@
+#ifndef DYNAMICOBJECT_H_
+#define DYNAMICOBJECT_H_
+
+#include "../../actionhelpers.h"
+
+obj_tag * DynamicObject_Create(_VECTOR *pos, _VECTOR *rot, level_tag *lvl, celglist_tag *param_4);
+
+#endif // DYTNAMICOBJECT_H_
--- src/action/game/obj/Env.cpp
+++ src/action/game/obj/Env.cpp
@@ -2,3 +2,6 @@
 
 // AUTOGEN
 void Env_Create(_VECTOR *pos, _VECTOR *rot, celglist_tag *celgl, level_tag *lvl);
+
+// AUTOGEN
+obj_tag* Env_Tree_Create(_VECTOR *pos, _VECTOR *rot, level_tag *lvl);
