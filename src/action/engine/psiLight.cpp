da60b8a30a977f155a373564319e0079338b35cc
Charlie Bruce <charliebruce@gmail.com>
2026-09-13 05:59:30 +0100

    Light_Create and Light_Update implemented

--- src/action/engine/psiLight.cpp
+++ src/action/engine/psiLight.cpp
@@ -1,7 +1,7 @@
 #include "psiLight.h"
 
 // AUTOGEN
-void* psiLight_Create(void);
+undefined4 __stdcall psiLight_Create(void);
 
 // AUTOGEN
 bool __stdcall psiLight_Delete(void);
--- src/action/engine/psiLight.h
+++ src/action/engine/psiLight.h
@@ -1,7 +1,9 @@
 #ifndef PSILIGHT_H
 #define PSILIGHT_H
 
-void* psiLight_Create(void);
+#include "../actionhelpers.h"
+
+undefined4 __stdcall psiLight_Create(void);
 bool __stdcall psiLight_Delete(void);
 
 #endif // PSILIGHT_H
