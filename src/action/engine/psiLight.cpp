5a32312aa630668b46f33600def601c3df5a2f19
Charlie Bruce <charliebruce@gmail.com>
2026-09-13 03:58:56 +0100

    Create placeholder psiLight

---
+++ src/action/engine/psiLight.cpp
@@ -0,0 +1,7 @@
+#include "psiLight.h"
+
+// AUTOGEN
+void* psiLight_Create(void);
+
+// AUTOGEN
+bool __stdcall psiLight_Delete(void);
---
+++ src/action/engine/psiLight.h
@@ -0,0 +1,7 @@
+#ifndef PSILIGHT_H
+#define PSILIGHT_H
+
+void* psiLight_Create(void);
+bool __stdcall psiLight_Delete(void);
+
+#endif // PSILIGHT_H
